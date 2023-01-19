cb2d75e789824ca530b96eb1f84462c41bcaeaf0
root <root@terraform.ru-central1.internal>
2023-01-19 19:07:45 +0300

    В файле create_yandex_key.sh изменено генерируемое имя файла по-умолчанию, чтобы оно совпадало с конфигурацией provider.tf. Удалено немного мусора

--- ya_network/example_show_current_subnets.sh
+++ ya_network/example_show_current_subnets.sh
@@ -1,4 +1,5 @@
 yc vpc subnets list
-read -p "Enter subnet to show info about it: " bash_subnet_id
-terraform refresh -var="current_subnet=$bash_subnet_id"
+# Later
+#read -p "Enter subnet to show info about it: " bash_subnet_id
+#terraform refresh -var="current_subnet=$bash_subnet_id"
 
